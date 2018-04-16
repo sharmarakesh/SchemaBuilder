b850ec1095795387d4f775e9918860c52287e64d
Rakesh Sharma <rakeshsharma.sharma@gmail.com>
2018-04-16 09:39:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+enum fields {
+  LABEL,
+  TEXT,
+  EMAIL,
+  PASSWORD,
+  TEXTAREA
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  myDynamicForm: FormGroup;
+  isFormCreated = false;
   title = 'app';
+  formGroups: any = {};
+  selectedFieldsName;
+  @ViewChild('playground')
+   private playground: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  createForm() {
+    this.myDynamicForm = new FormGroup(this.formGroups);
+    this.isFormCreated = true;
+
+  }
+  selectedFiled(event) {
+    this.selectedFieldsName = event.target.value;
+  }
+  createFormFields() {
+    if ( this.selectedFieldsName === fields.TEXT) {
+      this.formGroups.text = new FormControl();
+    }
+    const div = this.renderer.createElement('div');
+    const text = this.renderer.createText('This is div element');
+    this.renderer.appendChild(div, text);
+    this.renderer.appendChild(this.playground.nativeElement, div);
+  }
+
 }
